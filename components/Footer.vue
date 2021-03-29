429fbbee2f6f0bf8eb40e6d407d4dd5631217944
Raja s <rajaaltus@gmail.com>
2021-03-29 16:34:04 +0700

    menu and messager chat added

--- components/Footer.vue
+++ components/Footer.vue
@@ -21,9 +21,7 @@
             <div class="collapse dont-collapse-sm links" id="collapse_2">
               <ul>
                 <li>
-                  <nuxt-link to="/listing-grid-1-full"
-                    >Top Categories</nuxt-link
-                  >
+                  <nuxt-link to="/listing-grid-1-full">Top Categories</nuxt-link>
                 </li>
                 <li>
                   <nuxt-link to="/listing-grid-2-full">Best Rated</nuxt-link>
@@ -41,15 +39,9 @@
             <h3 data-target="#collapse_3">Contacts</h3>
             <div class="collapse dont-collapse-sm contacts" id="collapse_3">
               <ul>
-                <li>
-                  <i class="fa fa-map"></i>23/5 Khun bu lom Road, Sihom<br />Vientiane
-                  - Lao PDR
-                </li>
+                <li><i class="fa fa-map"></i>23/5 Khun bu lom Road, Sihom<br />Vientiane - Lao PDR</li>
                 <li><i class="fa fa-phone"></i>+856 20 580 26838</li>
-                <li>
-                  <i class="fa fa-envelope"></i
-                  ><a href="#0">info@rotihouselao.com</a>
-                </li>
+                <li><i class="fa fa-envelope"></i><a href="#0">info@rotihouselao.com</a></li>
               </ul>
             </div>
           </div>
@@ -58,20 +50,9 @@
             <div class="collapse dont-collapse-sm" id="collapse_4">
               <div id="newsletter">
                 <div id="message-newsletter"></div>
-                <form
-                  method="post"
-                  action="assets/newsletter.php"
-                  name="newsletter_form"
-                  id="newsletter_form"
-                >
+                <form method="post" action="assets/newsletter.php" name="newsletter_form" id="newsletter_form">
                   <div class="form-group">
-                    <input
-                      type="email"
-                      name="email_newsletter"
-                      id="email_newsletter"
-                      class="form-control"
-                      placeholder="Your email"
-                    />
+                    <input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email" />
                     <button type="submit" id="submit-newsletter">
                       <i class="fa fa-paper-plane"></i>
                     </button>
@@ -82,12 +63,7 @@
                 <h5>Follow Us</h5>
                 <ul>
                   <li>
-                    <a
-                      href="https://facebook.com/rotihouselao"
-                      target="_blank"
-                      class="text-light"
-                      ><i class="fa fa-facebook-f"></i
-                    ></a>
+                    <a href="https://facebook.com/rotihouselao" target="_blank" class="text-light"><i class="fa fa-facebook-f"></i></a>
                   </li>
                   <li>
                     <a class="text-light"><i class="fa fa-instagram"></i></a>
@@ -124,14 +100,7 @@
                 </div>
               </li>
               <li>
-                <img
-                  src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-                  data-src="img/cards_all.svg"
-                  alt=""
-                  width="198"
-                  height="30"
-                  class="lazy"
-                />
+                <img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="img/cards_all.svg" alt="" width="198" height="30" class="lazy" />
               </li>
             </ul>
           </div>
@@ -142,17 +111,17 @@
               <li><span>© 2019 RotiHouse</span></li>
             </ul>
           </div>
-          <button
-            type="button"
-            class="btn btn-danger scroll-to"
-            v-scroll-to="'body'"
-          >
+          <button type="button" class="btn btn-danger scroll-to" v-scroll-to="'body'">
             <i class="fa fa-angle-up"></i>
           </button>
         </div>
       </div>
     </footer>
     <div class="layer"></div>
+    <!-- Load Facebook SDK for JavaScript -->
+
+    <!-- Your Chat Plugin code -->
+
     <!-- Opacity Mask Menu Mobile -->
   </div>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <nuxt-link class="navbar-brand shadow-lg" to="/" style="background-color: #eee;">
-        <img src="/logo.svg" alt="" class="logo-style" />
+      <nuxt-link class="navbar-brand shadow-lg" to="/">
+        <img src="/logo_light.svg" alt="" />
       </nuxt-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -25,11 +25,30 @@
         </ul>
       </div>
     </div>
+    <div id="fb-root"></div>
+    <div class="fb-customerchat" attribution="setup_tool" page_id="565534410506390"></div>
   </nav>
 </template>
 
-<style>
-.logo-style {
-  background-color: #eeeeee !important;
-}
-</style>
+<script>
+export default {
+  mounted() {
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: "v10.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  },
+};
+</script>
